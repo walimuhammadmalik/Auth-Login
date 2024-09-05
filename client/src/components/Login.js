08efc5e9b8b525bcdc2a30b6a234bc90d6923ab7
Wali Muhammad <77934276+walimuhammadmalik@users.noreply.github.com>
2024-09-05 22:42:57 +0500

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
+// login 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
